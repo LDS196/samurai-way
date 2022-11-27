8110f3312b7ec05d96025453b6288c2d0493f627
LDS196 <lihachevd87@gmail.com>
2022-11-27 23:09:41 +0400

    init project

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+function Header() {
+    return(
+        <header className="header">
+            <img className="header__logo"  src="https://www.humanrightslogo.net/sites/default/files/HRLogoCMYKsmall.jpg" alt="logo"/>
+        </header>
+    )
+}
+export default Header;
---
+++ src/components/profile.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+function Profile() {
+    return(
+        <div className="content">
+            <div className="">
+                <img className="content__img" src="https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8=" alt="about"/>
+            </div>
+            <div className="">Ava</div>
+            <div className="">
+                <div className="qq">My Post</div>
+                <div className="ww">New Post</div>
+                <div className="rr">Post 1</div>
+                <div className="tt">Post 2</div>
+            </div>
+            <div className=""></div>
+            <div className=""></div>
+            <div className=""></div>
+        </div>
+    )
+}
+export default Profile;
